db6a0174e5aa75dc12ff5c575ffb743cf7a5d913
Mauricio Ramirez <mauricioramirez@MacBook-Pro-de-Mauricio.local>
2021-09-19 19:02:16 -0600

    add post skill details validation

--- src/main/java/maudev/portfolio/service/SkillDetailServiceImpl.java
+++ src/main/java/maudev/portfolio/service/SkillDetailServiceImpl.java
@@ -17,7 +17,7 @@ public class SkillDetailServiceImpl implements SkillDetailService{
 
     @Override
     public SkillDetail deleteSkillDetail(Long id) {
-        // TODO Auto-generated method stub
+       
 
         SkillDetail skill = findById(id);
 
@@ -30,7 +30,7 @@ public class SkillDetailServiceImpl implements SkillDetailService{
 
     @Override
     public List<SkillDetail> findAll() {
-        // TODO Auto-generated method stub
+    
         List<SkillDetail> skills = skillDetailRepository.findAll();
         if (skills == null) {
             return null;
@@ -41,14 +41,21 @@ public class SkillDetailServiceImpl implements SkillDetailService{
 
     @Override
     public SkillDetail findById(Long id) {
-        // TODO Auto-generated method stub
+    
         return skillDetailRepository.findById(id).orElse(null);
     }
 
     @Override
     public SkillDetail saveSkillDetail(SkillDetail skillDetail) {
-        // TODO Auto-generated method stub
+        if (skillDetail.getId() != null ) {
+            SkillDetail skillDetailDB = findById(skillDetail.getId());
 
+            if (skillDetailDB != null) {
+                return skillDetailDB;
+            }
+        }
+
+        
         skillDetail.setStatus("CREATED");
         skillDetail.setCreateat(new Date());
         return skillDetailRepository.save(skillDetail);
@@ -56,7 +63,7 @@ public class SkillDetailServiceImpl implements SkillDetailService{
 
     @Override
     public SkillDetail updateSkillDetail(SkillDetail skillDetail) {
-        // TODO Auto-generated method stub
+      
 
         SkillDetail skillDetailDB = findById(skillDetail.getId());
 
